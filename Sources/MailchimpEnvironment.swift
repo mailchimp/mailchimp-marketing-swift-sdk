fab85a09f4ce010677b6dd228bab0e675b5e2b99
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2026-09-16 10:42:26 -0400

    [fern-generated] Update SDK
    Generated by Fern
    CLI Version: unknown
    Generators:
      - fernapi/fern-swift-sdk: 0.36.1

--- Sources/MailchimpEnvironment.swift
+++ Sources/MailchimpEnvironment.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 public enum MailchimpEnvironment: String, CaseIterable {
-    case `default` = "https://us1.api.mailchimp.com"
+    case `default` = "https://api.mailchimp.com"
 }
--- Sources/Version.swift
+++ Sources/Version.swift
@@ -1,1 +1,1 @@
-public let sdkVersion = "0.0.122"
+public let sdkVersion = "0.0.202"
